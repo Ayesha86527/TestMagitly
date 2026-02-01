8426a5346583c88c9e651d786fd152fadc0bb61c
unknown <ayeshanoman490@gmail.com>
2026-02-01 12:44:05 +0500

    Add file download and log reading endpoints

---
+++ api.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, send_file, request
+
+app = Flask(__name__)
+
+@app.route('/download')
+def download_file():
+    filename = request.args.get('file')
+    filepath = os.path.join('uploads', filename)
+    return send_file(filepath)
+
+@app.route('/read_log')
+def read_log():
+    log_name = request.args.get('log')
+    log_path = '/var/logs/' + log_name
+    with open(log_path, 'r') as f:
+        return f.read()
